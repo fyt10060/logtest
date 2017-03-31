4b31ea51c63a0346d91b602a67c8eeb524d4c502
fuyutong <fyt2010w@163.com>
2017-03-31 16:52:13 +0800

    api version

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	//	"strings"
+	"encoding/json"
 	"io/ioutil"
 	"time"
 
@@ -28,40 +29,15 @@ const fileName string = "requestLog.txt"
 //	}
 //}
 
-func getLog(w http.ResponseWriter, r *http.Request) {
-	fl, err := os.Open(fileName)
-	if err != nil {
-		return
-	}
-	defer fl.Close()
-	buf, err := ioutil.ReadAll(fl)
-	if err != nil {
-		fmt.Println("file not exist")
-		return
-	}
-	if len(buf) == 0 {
-		fmt.Println("buf is empty")
-		return
-	} else {
-		fmt.Fprintf(w, string(buf))
-		return
-	}
+type ApiVersion struct {
+	Host         string `json: host`
+	Version      string `json: version`
+	AddonHost    string `json: addon_host`
+	AddonVersion string `json: addon_version`
 }
 
-func saveRequestToLog(w http.ResponseWriter, r *http.Request) {
-	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println("open file error")
-		return
-	}
-	r.ParseForm()
-	t := time.Now()
-
-	file.WriteString(t.Format("2006-01-02 15:04:06"))
-
-	file.WriteString(" ip:" + r.RemoteAddr)
-	file.WriteString("\n")
-	file.Close()
+type ApiVersionSlice struct {
+	apis []ApiVersion `json: data`
 }
 
 func main() {
@@ -100,6 +76,52 @@ func (c *mainController) Get() {
 		go saveRequestToLog(w, r)
 	}
 
+}
+
+func getApiAddress(w http.ResponseWriter) {
+	var s ApiVersionSlice
+	s.apis = append(s.apis, ApiVersion{Host: "apidev.weixinhost.com", Version: "/4/", AddonHost: "api.weixinhost.com", AddonVersion: "/3/"})
+	b, err := json.Marshal(s)
+	if err != nil {
+		return
+	}
+	fmt.Fprintln(w, string(b))
+}
+
+func getLog(w http.ResponseWriter, r *http.Request) {
+	fl, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	defer fl.Close()
+	buf, err := ioutil.ReadAll(fl)
+	if err != nil {
+		fmt.Println("file not exist")
+		return
+	}
+	if len(buf) == 0 {
+		fmt.Println("buf is empty")
+		return
+	} else {
+		fmt.Fprintf(w, string(buf))
+		return
+	}
+}
+
+func saveRequestToLog(w http.ResponseWriter, r *http.Request) {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("open file error")
+		return
+	}
+	r.ParseForm()
+	t := time.Now()
+
+	file.WriteString(t.Format("2006-01-02 15:04:06"))
+
+	file.WriteString(" ip:" + r.RemoteAddr)
+	file.WriteString("\n")
+	file.Close()
 }
 
 func httpNotFound(w http.ResponseWriter, r *http.Request) {

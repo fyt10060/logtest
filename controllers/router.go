5e671d26d239bf14ac1619ca8d93a142f9c63f08
fuyutong <fyt2010w@163.com>
2017-04-25 10:30:33 +0800

    caculate test

--- controllers/router.go
+++ controllers/router.go
@@ -4,8 +4,10 @@ package controllers
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 
 	"logtest/model"
+	"logtest/service"
 
 	"github.com/astaxie/beego"
 )
@@ -38,7 +40,25 @@ func (this *RouterController) Post() {
 		return
 	case model.TextMessage:
 		msg, _ := result.(model.TextMessage)
-		fmt.Println(msg.MsgId)
+		var content string
+		content = msg.Content
+		if msg.Content == "做算术" || msg.Content == "做算数" || msg.Content == "算术" || msg.Content == "算数" {
+			content, _ = doOperation(msg.Content)
+			service.SetDoingOperation(true)
+		} else if msg.Content == "结束" || msg.Content == "=" {
+			if service.CheckDoingOperation() {
+				content, _ = doOperation(msg.Content)
+				service.SetDoingOperation(false)
+			}
+		} else {
+			if service.CheckDoingOperation() {
+				contents, isOver := doOperation(msg.Content)
+				content = contents
+				if isOver {
+					service.SetDoingOperation(false)
+				}
+			}
+		}
 		rMsg := model.TextMessage{
 			Message: model.Message{
 				ToUser:     msg.FromUser,
@@ -46,7 +66,7 @@ func (this *RouterController) Post() {
 				MsgType:    model.MTText,
 				CreateTime: msg.CreateTime + 1,
 			},
-			Content: msg.Content,
+			Content: content,
 		}
 		response := model.GetResponseMessage(rMsg)
 		w.Write(response)
@@ -55,4 +75,31 @@ func (this *RouterController) Post() {
 		w.Write(response)
 	}
 
+}
+
+func doOperation(content string) (respContent string, isOver bool) {
+	switch content {
+	case "算术", "算数", "做算术", "做算数":
+		return "开始计算，请输入第一个数字", false
+	case "结束", "=":
+
+		return "已结束，你的结果是：呵呵哒", true
+	default:
+		if service.CheckShouldBeNum() {
+			num, err := strconv.ParseFloat(content, 10)
+			if err != nil {
+				fmt.Println(num)
+				return "请输入数字", false
+			}
+			service.SetShouldBeNumber(false)
+			return "请输入符号", false
+		} else {
+			if content == "+" || content == "-" || content == "*" || content == "/" {
+				service.SetShouldBeNumber(true)
+				return "请输入数字", false
+			} else {
+				return "请输入一个符号，目前仅支持'+''-''*''/'", false
+			}
+		}
+	}
 }
---
+++ service/redis_manager.go
@@ -0,0 +1,84 @@
+// redis_manager
+package service
+
+import (
+	"fmt"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+const (
+	keyIsDoingOperation = "isDoingOpertaion"
+	keyShouldBeNum      = "shouldBeNum"
+)
+
+var (
+	MaxPoolSize = 20
+	redisPool   chan redis.Conn
+)
+
+// redis pool related
+func putRedis(conn redis.Conn) {
+	if redisPool == nil {
+		redisPool = make(chan redis.Conn, MaxPoolSize)
+	}
+	if len(redisPool) >= MaxPoolSize {
+		conn.Close()
+		return
+	}
+	fmt.Printf("redis conn pool size: %d\n", len(redisPool))
+	redisPool <- conn
+}
+
+func InitRedis(address string) redis.Conn {
+	if len(redisPool) == 0 {
+		redisPool = make(chan redis.Conn, MaxPoolSize)
+		go func() {
+			for i := 0; i < MaxPoolSize/2; i++ {
+				c, err := redis.Dial("tcp", address)
+				if err != nil {
+					panic(err)
+				}
+				putRedis(c)
+			}
+		}()
+	}
+	return <-redisPool
+}
+
+func getRedisConn() redis.Conn {
+	return InitRedis("123.56.137.103:6379")
+}
+
+func CheckDoingOperation() bool {
+	return getBoolResult(keyIsDoingOperation, false)
+}
+
+func CheckShouldBeNum() bool {
+	return getBoolResult(keyShouldBeNum, true)
+}
+
+func getBoolResult(withKey string, nilValue bool) bool {
+	conn := getRedisConn()
+	defer putRedis(conn)
+	is, err := redis.Bool(conn.Do("get", withKey))
+	if err != nil {
+		return nilValue
+	} else {
+		return is
+	}
+}
+
+func setToRedisWithKey(key string, value interface{}) {
+	conn := getRedisConn()
+	defer putRedis(conn)
+	conn.Do("set", key, value)
+}
+
+func SetDoingOperation(set bool) {
+	setToRedisWithKey(keyIsDoingOperation, set)
+}
+
+func SetShouldBeNumber(set bool) {
+	setToRedisWithKey(keyShouldBeNum, set)
+}
